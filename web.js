794c3425c3fcfbd2124e1d23c5a8c6079f11aa02
Henry Szeto <ucla@hotmail.com>
2015-01-13 22:25:49 -0800

    removed logger from web.js for Heroku

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 // var public_path = require('path');
  
-app.use(express.logger('dev'));
+// app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 // app.use(express.static(public_path.join(__dirname, 'dist')));
 app.listen(process.env.PORT || 5000);
