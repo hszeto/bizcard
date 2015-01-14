d114b14a5672e065b91894995305cd344f30d3fc
Henry Szeto <ucla@hotmail.com>
2015-01-14 00:08:32 -0800

    update web.js for node server

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 // A simple Node server for Heroku deployment
 
-var gzippo = require('gzippo');
+// var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-// var public_path = require('path');
+var public_path = require('path');
  
 // app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-// app.use(express.static(public_path.join(__dirname, 'dist')));
+// app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(express.static(public_path.join(__dirname, 'dist')));
 app.listen(process.env.PORT || 5000);
